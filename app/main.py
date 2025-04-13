c9c2f13d51603feff7eb13cb4fa7e9820bb13071
KhanhNquyen <kqn2000k@gmail.com>
2025-04-13 16:36:16 +0700

    release app

--- app/main.py
+++ app/main.py
@@ -3,9 +3,9 @@ from fastapi.middleware.cors import CORSMiddleware
 from .routers import keyword_router, suggestion_router, web_search_router, verify_router, pipeline
 from pyngrok import ngrok
 
-HOST = "0.0.0.0"
-PORT = 8000
-USE_NGROK = True
+# HOST = "0.0.0.0"
+# PORT = 8000
+# USE_NGROK = True
 
 app = FastAPI(
     title="Movie recommendations",
@@ -35,13 +35,13 @@ api_router.include_router(pipeline.router)
 # Include the main router
 app.include_router(api_router)
 
-if USE_NGROK:
-    # https://dashboard.ngrok.com/get-started/your-authtoken
-    ngrok.set_auth_token("2OZzph05o2aDJV38FTwj5BbcQZl_6uNvCLppcLoeXAdoRoJ4K")
+# if USE_NGROK:
+#     # https://dashboard.ngrok.com/get-started/your-authtoken
+#     ngrok.set_auth_token("2OZzph05o2aDJV38FTwj5BbcQZl_6uNvCLppcLoeXAdoRoJ4K")
 
-    public_url = ngrok.connect(PORT).public_url
-    print(f"ngrok tunnel {public_url}")
+#     public_url = ngrok.connect(PORT).public_url
+#     print(f"ngrok tunnel {public_url}")
 
 @app.get("/")
-async def read_root():
+def read_root():
     return {"message": "INT6024 - Movie recommendations API"}
